6ae23eabc7ee4df621f180eb678a41119076e630
Annapurneshwari <risannapurneshwari@gmail.com>
2025-02-09 17:45:32 +0530

    BestTime.java

---
+++ src/com/codes/blind75/BestTime.java
@@ -0,0 +1,18 @@
+package com.codes.blind75;
+
+import java.util.*;
+
+public class BestTime 
+{
+	public int maxProfit(int[] prices) {
+        int profit = 0, left = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[left]) {
+                profit = Math.max(profit, prices[i] - prices[left]);
+            } else {
+                left = i;
+            }
+        }
+        return profit;
+    }
+}
